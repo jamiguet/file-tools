da83263169d321f18868003fefa9dfbb306c3394
Juan Amiguet-Vercher <jamiguet@gmail.com>
2012-08-03 09:20:12 +0200

    Added --test option for testing

--- extract.sh
+++ extract.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
+
+
+if [ $1 -a $1 == "--test" ]; then
+    EXIT=0
+    seq 10 > test.dat
+
+    ./extract.sh 2 5 test.dat > test2.dat
+
+    size=`wc -l test2.dat`
+    set -- $size
+    size=$1
+
+    if [ $size -eq 3 ]; then
+	echo "TEST 1: OK"
+    else
+	echo "Wrong number of lines in file"
+	EXIT=1
+    fi
+
+    rm test.dat test2.dat
+
+    exit $EXIT
+fi
+
+
 if [ $# -ne 3 ]; then
 echo "Usage extract.sh <start-line> <end-line> <file>"
+echo "Usage extract.sh --test"
 exit 1
 fi
 
--- slice.sh
+++ slice.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
 
+if [  $1 -a  $1 == "--test" ]; then
+    EXIT=0
+    seq 10 > test.dat
+    ./slice.sh 3 test.dat
+    
+    fileNum=`ls test.dat.* | wc -l`
+    if [ $fileNum -ne 4 ]; then
+	echo "Incorrect number of files made"
+	EXIT=1
+    else
+	echo "Test 1: OK"
+    fi
+
+    fileNum=` wc -l test.dat.* `
+    set -- $fileNum
+    fileNum=$1
+    if [ $fileNum -ne 3 ]; then
+	echo "Incorrect number of lines in file"
+	EXIT=1
+    else
+	echo "Test 2: OK"
+    fi
+
+    rm test.dat*
+
+    exit $EXIT
+fi
+
+
 if [ $# -ne 2 ]; then
-	echo "usage: slice.sh <slice-size> <filesize>"
+	echo "usage: slice.sh <slice-size> <filename>"
+	echo "usage: slice.sh --test"
 	exit 1
 fi
+
+
 
 sliceSize=$1
 filename=$2
